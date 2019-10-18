4090f696752b87ede1f74a5ef892ba0632b56f76
realestindigo <jLoupias@roc-dev.com>
2019-10-18 07:53:15 +0100

    opdracht 2 t/m 5

--- PHPChallenge/opdracht2.php
+++ PHPChallenge/opdracht2.php
@@ -1,1 +1,50 @@
-<?php
+<!D0CTYPE html>
+<html>
+<head>
+    <title>PHP Challenge</title>
+    <link href="https://fonts.googleapis.com/css?family=Bangers&display=swap" rel="stylesheet">
+    <style>
+
+        body {
+            flex: ;
+            text-align: center;
+            list-style: none;
+            font-family: Arial;
+        }
+        .oranje {
+            text-align: center;
+            color: #ff8400;
+            font-family: 'Bangers', cursive;
+            font-size: 25px;
+        }
+        a {
+            color: inherit;
+            text-decoration: none;
+        }
+
+
+
+    </style>
+</head>
+<body>
+<img src="img/logos/monkey-business.jpg">
+
+<h1>SELECT YOUR MONKEY!</h1>
+
+<img src="img/logos/monkey_swings.png">
+<br>
+
+<div class="oranje">
+    <a href="https://www.google.nl/search?q=Baviaan&tbm=isch">BAVIAAN</a> <br>
+    <a href="https://www.google.nl/search?q=Guereza&tbm=isch">GUEREZA</a> <br>
+    <a href="https://www.google.nl/search?q=Langoer&tbm=isch">LANGOER</a> <br>
+    <a href="https://www.google.nl/search?q=Neusaap&tbm=isch">NEUSAAP</a> <br>
+    <a href="https://www.google.nl/search?q=Tamarin&tbm=isch">TAMARIN</a> <br>
+    <a href="https://www.google.nl/search?q=Brulaap&tbm=isch">BRULAAP</a> <br>
+    <a href="https://www.google.nl/search?q=Halfaap&tbm=isch">HALFAAP</a> <br>
+    <a href="https://www.google.nl/search?q=Madril&tbm=isch"> MANDRIL</a> <br>
+</div>
+
+</body>
+</html>
+
---
+++ PHPChallenge/opdracht5.php
@@ -0,0 +1,16 @@
+<!D0CTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<form>
+
+    Voeg een leefgebied toe :
+    <input type="text" name="leefgebied"><br>
+    <input type="submit">
+
+</form>
+
+</body>
+</html>
